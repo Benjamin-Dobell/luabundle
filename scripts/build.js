52e7ac24e0df1bde893bfe43c96e65446a60b746
Benjamin Dobell <benjamin.dobell+git@glassechidna.com.au>
2020-01-21 21:54:53 +1100

    Fix source maps

--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,9 @@
 const {spawn} = require('child_process')
+const path = require('path')
 const process = require('process')
 
 const fs = require('fs-extra')
+const klawSync = require('klaw-sync')
 const rimraf = require('rimraf')
 
 const copy = require('./utils/copy')
@@ -11,14 +13,27 @@ rimraf.sync(dist('*'), {glob: {dot: true}})
 
 process.chdir(root())
 
+const sourceRegex = new RegExp(`(sources":\\[")(?:\\.\\.${path.sep}\\.\\.${path.sep})((?:\\.\\.\\${path.sep})*src)`)
+
+// Surely there's some way to make tsconfig play nice. Ah well...
+function relocateJs() {
+	for (const item of klawSync(dist('src'), {filter: item => item.path.endsWith(".map"), traverseAll: true})) {
+		const content = fs.readFileSync(item.path, "utf8")
+		fs.writeFileSync(item.path, content.replace(sourceRegex, (_, prefix, suffix) => prefix + suffix))
+	}
+
+	fs.copySync(dist('src/'), dist())
+	rimraf.sync(dist('src'))
+}
+
 const tsc = root('node_modules', '.bin', 'tsc')
 spawn(tsc, ['-b'], { stdio: 'inherit' }).on('exit', code => {
 	if (code !== 0) {
 		process.exit(code)
 	}
 
-	fs.copySync(dist('src/'), dist())
-	rimraf.sync(dist('src'))
+
+	relocateJs()
 
 	copy([
 		['src/bundle/runtime.lua', 'bundle/runtime.lua'],
